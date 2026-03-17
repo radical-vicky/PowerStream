285a18b92be12009f6954fe77d9ef1196de169dc
Victor Odhiambo <146205165+radical-vicky@users.noreply.github.com>
2026-03-17 11:55:28 +0300

    Update settings.py

--- video_platform/settings.py
+++ video_platform/settings.py
@@ -75,12 +75,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'video_platform.wsgi.application'
 
-# Database
+import dj_database_url
+
+# Replace your existing DATABASES with this:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # Password validation
