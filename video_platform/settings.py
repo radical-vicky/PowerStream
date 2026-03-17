980544e3fb6ae38e284bed13a6c04104fb68af62
Victor Odhiambo <146205165+radical-vicky@users.noreply.github.com>
2026-03-17 03:23:59 +0300

    Update ALLOWED_HOSTS to include new domain

--- video_platform/settings.py
+++ video_platform/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-kxfni-8k_(r%6ck!go46d$8i4@9_lrv!g4$*w0@k*l%log8a@x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['powerstreamv.onrender.com','powerstream.onrender.com','powerstreamvideo', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['powerstreamv.onrender.com','powerstream.onrender.com','powerstreamvideo.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
